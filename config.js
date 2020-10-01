c72c87d57143f07eac06b04bd7191cb02f379348
unknown <vlasis.tassios7@gmail.com>
2020-10-01 13:04:35 +0300

    variable changes

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
-require("dotenv").config();
+require("dotenv").config(); //load variables from .env
 
+//pool is used to make queries to the db
 const { Pool } = require("pg");
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production"; //different behavior between environments in heroku
 
+//follow the pattern
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const express = require("express");
+/*
+ * entry point for server
+ *
+ */
+
+const express = require("express"); //import express
 const bodyParser = require("body-parser");
-const cors = require("cors");
+const cors = require("cors"); //connect/express middleware
 const { pool } = require("./config");
 
-const app = express();
+const app = express(); //access to express
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 const getEmployees = (request, response) => {
-  pool.query("SELECT * FROM dept", (error, results) => {
+  pool.query("SELECT * FROM employee", (error, results) => {
     if (error) {
       throw error;
     }
@@ -19,7 +24,7 @@ const getEmployees = (request, response) => {
 };
 
 const addEmployee = (request, response) => {
-  const { author, title } = request.body;
+  const { id, name, username, birth_day, sex, salary } = request.body;
 
   pool.query(
     "INSERT INTO employee (id , name , username , birth_day , sex , salary) VALUES($1, $2, $3, $4, $5, $6)",
